0e5788885bac5414297630f61c2070663fd56b67
kmanoj03 <manoj03.work@gmail.com>
2025-11-25 01:01:26 -0800

    checking status of PR after config fix

--- server/server.ts
+++ server/server.ts
@@ -3,4 +3,6 @@ import { runCli } from './routes/cliRoutes';
 runCli();
 
 //Check for failing github actions in a new branch after enabling branch protection rules.
-//Enagled the enforcement of the github actions workflow.
+//Enabled the enforcement of the github actions workflow.
+
+//Removed bad configs for this branch as well.
