e1324ee81f7bc2de82d4d05241cf8cb82f63b11d
kmanoj03 <manoj03.work@gmail.com>
2025-11-25 00:33:10 -0800

    enforcement added

--- server/server.ts
+++ server/server.ts
@@ -3,3 +3,4 @@ import { runCli } from './routes/cliRoutes';
 runCli();
 
 //Check for failing github actions in a new branch after enabling branch protection rules.
+//Enagled the enforcement of the github actions workflow.
