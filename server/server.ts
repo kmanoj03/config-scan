5472b6cdcb00886a63bb519656a3756b6c7e9890
kmanoj03 <manoj03.work@gmail.com>
2025-11-25 00:24:04 -0800

    added branch protection rules

--- server/server.ts
+++ server/server.ts
@@ -2,4 +2,4 @@ import { runCli } from './routes/cliRoutes';
 
 runCli();
 
-//Check for failing github actions in a new branch.
+//Check for failing github actions in a new branch after enabling branch protection rules.
